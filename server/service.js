93171ab7a2730d8dfe7f52118955dcc5212f2218
Nishanth Reddy <reddynishanth16@gmail.com>
2017-06-17 20:07:53 -0700

    Time Service Initial Commit

---
+++ server/service.js
@@ -0,0 +1,40 @@
+/**
+ * Created by Nishanth Reddy on 4/2/2017.
+ */
+
+'use strict';
+
+const express = require('express');
+const service = express();
+const request = require('superagent');
+const moment = require('moment');
+
+service.get('/service/:location', (req, res, next) => {
+
+    request.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + req.params.location +
+        '&key=AIzaSyB-9FWTqG1jKUq8bWwnILql8tzBQ1AADm8', (err, response) => {
+
+        if (err) {
+            console.log(err);
+            return res.sendStatus(500);
+        }
+
+        const location = response.body.results[0].geometry.location;
+        const timestamp = +moment().format('X');
+
+        request.get('https://maps.googleapis.com/maps/api/timezone/json?location=' + location.lat + ',' + location.lng +
+            '&timestamp=' + timestamp +'&key=AIzaSyCslGsd4VYVyqUzgoa0W1nVDTF9fu5GTW8', (err, response) =>{
+
+            if (err) {
+                console.log(err);
+                return res.sendStatus(500);
+            }
+
+            const result = response.body;
+            const timeString = moment.unix(timestamp + result.dstOffset + result.rawOffset).utc().format('dddd, MMMM Do YYYY, h:mm:ss a');
+            res.json({result: timeString});
+        });
+    });
+})
+
+module.exports = service;
